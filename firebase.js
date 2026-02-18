8e2a21a3109c59c609bf60b11cbc806ce475650d
Pradeep <pradeepsinghkundra01@gmail.com>
2026-02-18 11:25:46 +0530

    Update Firebase configuration and initialize Firestore

--- firebase.js
+++ firebase.js
@@ -1,21 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: API_KEY,
+  apiKey: "AIzaSyCBiQ0gDPdl2AXexDqp0olpR_BiFplaQZM",
   authDomain: "saavn-github.firebaseapp.com",
   projectId: "saavn-github",
   storageBucket: "saavn-github.firebasestorage.app",
-  messagingSenderId: SENDER_ID,
-  appId: APP_ID,
-  measurementId: MEASURE_ID,
+  messagingSenderId: "212533131865",
+  appId: "1:212533131865:web:02dfb66400fb7b61278f48",
+  measurementId: "G-C748BVMQFF",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
